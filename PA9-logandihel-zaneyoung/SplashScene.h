d8b4f6b31c8a4969495720073d9cf45ad6bf926e
Zane Young <Zane@iamayoung.com>
2018-04-13 11:49:19 -0700

    changed how Scenes happen in the game class and made the splash screen actually work

--- PA9-logandihel-zaneyoung/SplashScene.h
+++ PA9-logandihel-zaneyoung/SplashScene.h
@@ -1,10 +1,41 @@
 #pragma once
 #include "Scene.h"
 #include "SplashScreen.h"
+#include "PlayScene.h"
+#include "Game.h"
 
+extern Scene * CurrentScene;
 class SplashScene : public Scene {
+protected:
+	
 public:
-	SplashScene() : Scene() {
-		drawables.push_back(new SplashScreen());
+	virtual void update() {
+		cout << fCount << endl;
+		fCount++;
+		if (fCount >= (15 * 3)) {
+			text->setScale(sf::Vector2f(1 - ((float)(fCount - 45) / 45.0f), 1 - ((float)(fCount - 45) / 45.0f)));
+		}
+		if (fCount == (30*3)) {
+			CurrentScene = new PlayScene;
+			delete this;
+		}
+
 	}
+	SplashScene() : Scene() {
+		fCount = 0;
+		font.loadFromFile("./fonts/slope-opera/SlopeOpera.otf");
+		text = new sf::Text();
+		text->setFont(font);
+		text->setCharacterSize(50);
+		text->setString("Splash Screen!");
+		text->setPosition(sf::Vector2f(200, 200));
+
+		// remember that order matters! 
+		// what's pushed first is drawn first
+		drawables.push_back(text);
+	}
+private:
+	int fCount;
+	sf::Font font;
+	sf::Text *text;
 };
--- PA9-logandihel-zaneyoung/SplashScreen.h
+++ PA9-logandihel-zaneyoung/SplashScreen.h
@@ -4,14 +4,4 @@
 class SplashScreen : public sf::Drawable{
 public:
 private:
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-		cout << "drawing" << endl;
-		sf::Font font;
-		sf::Text text("HELLO WORLD AND THINGS...", font);
-		text.setCharacterSize(30);
-		text.setPosition(sf::Vector2f(200, 200));
-		text.setColor(sf::Color::Red);
-		text.setStyle(sf::Text::Bold);
-		target.draw(text);
-	}
 };
